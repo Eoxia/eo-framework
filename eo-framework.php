e1231c1c1ce5f37ba78f9aa5d7feaf581d1eb1cb
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-10-20 15:45:58 +0200

    1.0.0: Empèche l'inclusion à plusieurs reprises.

--- eo-framework.php
+++ eo-framework.php
@@ -11,11 +11,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-DEFINE( 'PLUGIN_EO_FRAMEWORK_PATH', realpath( plugin_dir_path( __FILE__ ) ) . '/' );
-DEFINE( 'PLUGIN_EO_FRAMEWORK_URL', plugins_url( basename( __DIR__ ) ) . '/' );
-DEFINE( 'PLUGIN_EO_FRAMEWORK_DIR', basename( __DIR__ ) );
+if ( ! class_exists( '\eoxia\Init_Util' ) ) {
+	DEFINE( 'PLUGIN_EO_FRAMEWORK_PATH', realpath( plugin_dir_path( __FILE__ ) ) . '/' );
+	DEFINE( 'PLUGIN_EO_FRAMEWORK_URL', plugins_url( basename( __DIR__ ) ) . '/' );
+	DEFINE( 'PLUGIN_EO_FRAMEWORK_DIR', basename( __DIR__ ) );
 
-require_once( 'core/class/singleton.class.php' );
-require_once( 'core/class/init.class.php' );
+	require_once( 'core/class/singleton.class.php' );
+	require_once( 'core/class/init.class.php' );
 
-Init_util::g()->exec( PLUGIN_EO_FRAMEWORK_PATH, basename( __FILE__, '.php' ) );
+	Init_Util::g()->exec( PLUGIN_EO_FRAMEWORK_PATH, basename( __FILE__, '.php' ) );
+}
