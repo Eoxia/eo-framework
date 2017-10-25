e5861a5a677ce8616e285d0a795926cc0f544b88
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-10-25 09:30:08 +0200

    1.5.0: Ajout d'une méthode pour gérer les dates

--- core/class/date.class.php
+++ core/class/date.class.php
@@ -71,5 +71,33 @@ if ( ! class_exists( '\eoxia\Date_Util' ) ) {
 
 			return mysql2date( $format, $date );
 		}
+
+		/**
+		 * Convertis les minutes en un format spécial sur 7h = 1 jour.
+		 *
+		 * @since 1.0.0
+		 * @version 1.0.0
+		 *
+		 * @param  integer $min               Le nombre de minute.
+		 * @param  boolean $display_full_min  Si oui, affiches $min entre paranthèse.
+		 *
+		 * @return string                     La date formatée.
+		 */
+		public function convert_to_custom_hours( $min, $display_full_min = true ) {
+			$minut_for_one_day = \eoxia\Config_Util::$init['eo-framework']->hour_equal_one_day * 60;
+			$day = intval( $min / $minut_for_one_day );
+
+			$sub_min = $min - ( $day * $minut_for_one_day );
+			$hour = intval( $sub_min / 60 );
+			$clone_min = intval( $sub_min - ( $hour * 60 ) );
+
+			$display = $day . 'j ' . $hour . 'h ' . $clone_min . 'min';
+
+			if ( $display_full_min ) {
+				$display .= ' (' . $min . 'min)';
+			}
+
+			return $display;
+		}
 	}
 } // End if().
