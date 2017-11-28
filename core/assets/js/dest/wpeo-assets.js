137fe68384bafc954d1a0d74dce9dd2d6424f944
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2017-11-28 11:28:12 +0100

    1.0.0: Ajout du loader

--- core/assets/js/dest/wpeo-assets.js
+++ core/assets/js/dest/wpeo-assets.js
@@ -480,11 +480,17 @@ if ( ! window.eoxiaJS.loader ) {
 
 	window.eoxiaJS.loader.display = function( element ) {
 		element.addClass( 'wpeo-loader' );
+
+		var el = jQuery( '<span class="loader-spin"></span>' );
+		element[0].loaderElement = el;
+		element.append( element[0].loaderElement );
 	};
 
 	window.eoxiaJS.loader.remove = function( element ) {
 		if ( 0 < element.length ) {
 			element.removeClass( 'wpeo-loader' );
+
+			jQuery( element[0].loaderElement ).remove();
 		}
 	};
 }
