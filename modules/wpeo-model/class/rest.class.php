4aa60123b7eb83fbfcce9f09c766c0686a9c800a
Jimmy Eoxia <jimmy.eoxia@gmail.com>
2018-01-05 16:13:03 +0100

    1.0.0: Ajout route avec parent

--- modules/wpeo-model/class/rest.class.php
+++ modules/wpeo-model/class/rest.class.php
@@ -114,6 +114,19 @@ if ( ! class_exists( '\eoxia\Rest_Class' ) ) {
 				),
 			), true );
 
+			register_rest_route( $element_namespace->getNamespaceName() . '/v' . Config_Util::$init['eo-framework']->wpeo_model->api_version , '/' . $this->base . 's/(?P<id>[\d]+)', array(
+				array(
+					'method' => \WP_REST_Server::READABLE,
+					'callback'	=> array( $this, 'get_parent_from_parent' ),
+					'permission_callback' => function() {
+						if ( ( ! in_array( $_SERVER['REMOTE_ADDR'], Config_Util::$init['eo-framework']->wpeo_model->allowed_ip_for_unauthentified_access_rest, true ) ) && ! $this->check_cap( 'get' ) ) {
+							return false;
+						}
+						return true;
+					},
+				),
+			), true );
+
 		}
 
 		/**
@@ -132,6 +145,29 @@ if ( ! class_exists( '\eoxia\Rest_Class' ) ) {
 
 			if ( ! empty( $request ) && ( ! empty( $request['id'] ) ) ) {
 				$args['id'] = $request['id'];
+				$single = true;
+			}
+
+			return $this->get( $args, $single );
+		}
+
+		/**
+		 * Get element(s) from parent object type
+		 *
+		 * @since 1.5.1
+		 * @version 1.5.1
+		 *
+		 * @param  WP_Http::request $request The current Rest API request.
+		 *
+		 * @return mixed                     Element list or single element if id was specified.
+		 */
+		public function get_parent_from_parent( $request ) {
+			$args = array();
+			$single = false;
+
+			if ( ! empty( $request ) && ( ! empty( $request['id'] ) ) ) {
+				$args['post_id'] = $request['id'];
+				$args['status'] = '-34070';
 				$single = true;
 			}
 
