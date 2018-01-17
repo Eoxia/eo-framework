a2b35d181ef266189c647ed66b718b6ca27536e3
eoxia-jimmy <jimmy.eoxia@gmail.com>
2018-01-17 17:27:29 +0100

    1.0.0: the func handle_data is now more easy to understand.

--- modules/wpeo-model/class/constructor_data.class.php
+++ modules/wpeo-model/class/constructor_data.class.php
@@ -3,10 +3,10 @@
  * Gestion de la construction des données selon les modèles.
  *
  * @author Jimmy Latour <dev@eoxia.com>
- * @since 1.0.0
- * @version 1.6.0
- * @copyright 2015-2017
- * @package WPEO_Model
+ * @since 0.1.0
+ * @version 1.0.0
+ * @copyright 2015-2018
+ * @package EO_Framework
  */
 
 namespace eoxia;
@@ -25,64 +25,63 @@ if ( ! class_exists( '\eoxia\Constructor_Data_Class' ) ) {
 		 * Appelle la méthode pour dispatcher les données.
 		 *
 		 * @since 1.0.0
-		 * @version 1.4.0
+		 * @version 1.0.0
 		 *
 		 * @param Array $data Les données en brut.
 		 */
 		public function __construct( $data ) {
-			$this->dispatch_wordpress_data( $data, $data );
+			$this->handle_data( $data );
 		}
 
 		/**
 		 * Dispatches les données selon le modèle.
 		 *
 		 * @since 1.0.0
-		 * @version 1.6.0
+		 * @version 1.0.0
 		 *
-		 * @param array  $all_data       Toutes les données.
-		 * @param array  $data           Les données actuelles.
+		 * @param array  $data           Toutes les données.
+		 * @param array  $current_data   Les données actuelles.
 		 * @param object $current_object L'objet en cours de construction.
 		 * @param array  $model          La définition des données.
 		 * @return object
 		 */
-		private function dispatch_wordpress_data( $all_data, $data, $current_object = null, $model = array() ) {
-			if ( empty( $model ) ) {
-				$model = $this->model;
-			}
-
-			if ( null === $current_object ) {
-				$current_object = $this;
-			}
+		private function handle_data( $data, $current_data = null, $current_object = null, $model = null ) {
+			$model          = ( null === $model ) ? $this->model : $model;
+			$current_object = ( null === $current_object ) ? $this : $current_object;
+			$current_data   = ( null === $current_data ) ? $data : $current_data;
 
 			foreach ( $model as $field_name => $field_def ) {
-				if ( is_object( $current_object ) ) {
-					$current_object->$field_name = $this->set_default_data( $field_name, $field_def );
-				} else {
-					$current_object[ $field_name ] = $this->set_default_data( $field_name, $field_def );
-				}
+				// Définie les données  par défaut pour l'élément courant par rapport à "bydefault".
+				$value = $this->set_default_data( $field_name, $field_def );
 
-				// Est-ce qu'il existe des enfants ?
-				if ( isset( $field_def['field'] ) && isset( $data[ $field_def['field'] ] ) && ! isset( $field_def['child'] ) ) {
-					$current_object->$field_name = $data[ $field_def['field'] ];
-				} elseif ( isset( $field_def['child'] ) ) {
-					$current_data = ! empty( $all_data[ $field_name ] ) ? $all_data[ $field_name ] : array();
+				// Si la définition de la donnée ne contient pas "child".
+				if ( ! isset( $field_def['child'] ) ) {
+
+					// Si on est au premier niveau de $current_object, sinon si on est plus haut que le premier niveau.
+					if ( isset( $field_def['field'] ) && isset( $data[ $field_def['field'] ] ) ) {
+						$value = $data[ $field_def['field'] ];
+					} elseif ( isset( $data[ $field_name ] ) && isset( $field_def ) && ! isset( $field_def['child'] ) ) {
+						$value = $data[ $field_name ];
+					}
+
+					// Enregistres la valeur soit dans un objet, ou alors dans un tableau.
+					if ( is_object( $current_object ) ) {
+						$current_object->$field_name = $value;
+					} else {
+						$current_object[ $field_name ] = $value;
+					}
+				} else {
+					$current_data = ! empty( $data[ $field_name ] ) ? $data[ $field_name ] : array();
 
 					if ( empty( $current_object->$field_name ) ) {
 						$current_object->$field_name = new \stdClass();
 					}
 
-					$current_object->$field_name = $this->dispatch_wordpress_data( $all_data, $current_data, $current_object->$field_name, $field_def['child'] );
+					// Récursivité sur les enfants de la définition courante.
+					$current_object->$field_name = $this->handle_data( $data, $current_data, $current_object->$field_name, $field_def['child'] );
 				}
 
-				// $field_name existe est n'a pas d'enfant.
-				if ( isset( $data[ $field_name ] ) && isset( $field_def ) && ! isset( $field_def['child'] ) ) {
-					if ( is_object( $current_object ) ) {
-						$current_object->$field_name = $data[ $field_name ];
-					} else {
-						$current_object[ $field_name ] = $data[ $field_name ];
-					}
-				}
-
+				// Force le typage.
 				$current_object = $this->handle_type( $current_object, $field_def, $field_name );
 			}
 
@@ -93,12 +92,12 @@ if ( ! class_exists( '\eoxia\Constructor_Data_Class' ) ) {
 		 * Si la définition bydefault existe, récupères la valeur.
 		 *
 		 * @since 1.0.0
-		 * @version 1.5.0
+		 * @version 1.0.0
 		 *
 		 * @param string $field_name Le nom du champ.
 		 * @param array  $field_def  La définition du champ.
 		 *
-		 * @return mixed						 La donnée par défaut.
+		 * @return mixed             La donnée par défaut.
 		 */
 		private function set_default_data( $field_name, $field_def ) {
 			if ( 'wpeo_date' === $field_def['type'] ) {
@@ -114,7 +113,7 @@ if ( ! class_exists( '\eoxia\Constructor_Data_Class' ) ) {
 		 * Convertis le modèle en un tableau compatible WordPress.
 		 *
 		 * @since 1.0.0
-		 * @version 1.5.0
+		 * @version 1.0.0
 		 *
 		 * @return array Tableau compatible avec les fonctions WordPress.
 		 */
@@ -122,9 +121,11 @@ if ( ! class_exists( '\eoxia\Constructor_Data_Class' ) ) {
 			$data = array();
 
 			foreach ( $this->model as $field_name => $field_def ) {
+
+				// Le champ est de type "wpeo_date", on le repasse au format mysql pour l'enregistrement.
 				if ( 'wpeo_date' === $field_def['type'] && ! empty( $field_def['field'] ) ) {
 					$data[ $field_def['field'] ] = $this->{$field_name}['date_input']['date'];
-					$this->{$field_name} = $this->{$field_name}['date_input']['date'];
+					$this->{$field_name}         = $this->{$field_name}['date_input']['date'];
 				}
 			}
 
@@ -141,6 +142,17 @@ if ( ! class_exists( '\eoxia\Constructor_Data_Class' ) ) {
 			return $data;
 		}
 
+		/**
+		 * Forces le typage des données.
+		 *
+		 * @since 1.0.0
+		 * @version 1.0.0
+		 *
+		 * @param  Object $current_object L'objet courant.
+		 * @param  array  $field_def      La définition du champ.
+		 * @param  string $field_name     Le nom du champ.
+		 * @return Object                 L'objet avec le typage forcé.
+		 */
 		public function handle_type( $current_object, $field_def, $field_name ) {
 			$data = is_object( $current_object ) ? $current_object->$field_name : $current_object[ $field_name ];
 			if ( ! empty( $field_def['type'] ) ) {
@@ -158,8 +170,10 @@ if ( ! class_exists( '\eoxia\Constructor_Data_Class' ) ) {
 						}
 					}
 				} else {
+
+					// Si c'est un type "wpeo_date".
 					$current_time = ! empty( $current_object->{$field_name}['date_input'] ) && ! empty( $current_object->{$field_name}['date_input']['date'] ) ? $current_object->{$field_name}['date_input']['date'] : $current_object->$field_name;
-					$data = $this->fill_date( $current_time );
+					$data         = $this->fill_date( $current_time );
 				}
 			}
 
@@ -175,8 +189,8 @@ if ( ! class_exists( '\eoxia\Constructor_Data_Class' ) ) {
 		/**
 		 * Remplis les champs de type 'wpeo_date'.
 		 *
-		 * @since 1.5.0
-		 * @version 1.5.0
+		 * @since 1.0.0
+		 * @version 1.0.0
 		 *
 		 * @param  string $current_time Le date envoyé par l'objet.
 		 * @return array {
@@ -206,32 +220,32 @@ if ( ! class_exists( '\eoxia\Constructor_Data_Class' ) ) {
 			$data = array();
 
 			$locale = get_locale();
-			$date = new \DateTime( $current_time );
+			$date   = new \DateTime( $current_time );
 
-			$data['date_input']['date'] = $current_time;
+			$data['date_input']['date']    = $current_time;
 			$data['date_input']['iso8601'] = mysql2date( 'Y-m-d\TH:i:s\Z', $current_time );
 
-			$data['date_input']['fr_FR']['date'] = mysql2date( 'd/m/Y', $current_time );
+			$data['date_input']['fr_FR']['date']      = mysql2date( 'd/m/Y', $current_time );
 			$data['date_input']['fr_FR']['date_time'] = mysql2date( 'd/m/Y H:i:s', $current_time );
-			$data['date_input']['fr_FR']['time'] = mysql2date( 'H:i:s', $current_time );
+			$data['date_input']['fr_FR']['time']      = mysql2date( 'H:i:s', $current_time );
 
-			$data['date_input']['en_US']['date'] = mysql2date( 'm-d-y', $current_time );
+			$data['date_input']['en_US']['date']      = mysql2date( 'm-d-y', $current_time );
 			$data['date_input']['en_US']['date_time'] = mysql2date( 'm-d-y H:i:s', $current_time );
-			$data['date_input']['en_US']['time'] = mysql2date( 'H:i:s', $current_time );
+			$data['date_input']['en_US']['time']      = mysql2date( 'H:i:s', $current_time );
 
-			$data['mysql'] = $current_time;
+			$data['mysql']   = $current_time;
 			$data['iso8601'] = mysql_to_rfc3339( $current_time );
 
-			$formatter = new \IntlDateFormatter( $locale, \IntlDateFormatter::SHORT, \IntlDateFormatter::NONE );
+			$formatter    = new \IntlDateFormatter( $locale, \IntlDateFormatter::SHORT, \IntlDateFormatter::NONE );
 			$data['date'] = $formatter->format( $date );
 
-			$formatter = new \IntlDateFormatter( $locale, \IntlDateFormatter::SHORT, \IntlDateFormatter::SHORT );
+			$formatter         = new \IntlDateFormatter( $locale, \IntlDateFormatter::SHORT, \IntlDateFormatter::SHORT );
 			$data['date_time'] = $formatter->format( $date );
 
-			$formatter = new \IntlDateFormatter( $locale, \IntlDateFormatter::NONE, \IntlDateFormatter::SHORT );
+			$formatter    = new \IntlDateFormatter( $locale, \IntlDateFormatter::NONE, \IntlDateFormatter::SHORT );
 			$data['time'] = $formatter->format( $date );
 
-			$formatter = new \IntlDateFormatter( $locale, \IntlDateFormatter::FULL, \IntlDateFormatter::SHORT );
+			$formatter                   = new \IntlDateFormatter( $locale, \IntlDateFormatter::FULL, \IntlDateFormatter::SHORT );
 			$data['date_human_readable'] = \ucwords( $formatter->format( $date ) );
 
 			return apply_filters( 'eoframework_fill_date', $data );
