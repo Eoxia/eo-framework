421278182242bfcfa719985ba074569e585d2af7
Alexandre Techer - Eoxia <alexandre.techer@eoxia.com>
2018-02-13 11:27:33 +0100

    1.0.0: Ajout de datetimepicker

--- core/action/core.action.php
+++ core/action/core.action.php
@@ -45,8 +45,10 @@ class Core_Action {
 	public function callback_mixed_enqueue_scripts() {
 		wp_register_script( 'wpeo-assets-scripts', Config_Util::$init['eo-framework']->core->url . 'assets/js/dest/wpeo-assets.js', array( 'jquery' ), \eoxia\Config_Util::$init['eo-framework']->version, false );
 		wp_register_script( 'wpeo-assets-fontawesome', Config_Util::$init['eo-framework']->core->url . 'assets/js/dest/font/fontawesome-all.min.js', array( 'jquery' ), \eoxia\Config_Util::$init['eo-framework']->version, false );
+		wp_enqueue_script( 'wpeo-assets-datepicker-js', Config_Util::$init['eo-framework']->core->url . 'assets/js/dest/jquery.datetimepicker.full.js', array( 'jquery' ), \eoxia\Config_Util::$init['eo-framework']->version, false );
 
 		wp_enqueue_style( 'wpeo-assets-styles', Config_Util::$init['eo-framework']->core->url . 'assets/css/style.min.css', \eoxia\Config_Util::$init['eo-framework']->version );
+		wp_enqueue_style( 'wpeo-assets-datepicker', Config_Util::$init['eo-framework']->core->url . 'assets/css/jquery.datetimepicker.css', array(), \eoxia\Config_Util::$init['eo-framework']->version );
 
 		wp_localize_script( 'wpeo-assets-scripts', 'wpeo_framework', Core_Class::g()->get_localize_script_data() );
 		wp_enqueue_script( 'wpeo-assets-scripts' );
