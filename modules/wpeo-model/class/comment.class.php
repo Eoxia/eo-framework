618327cd3842ddba147630a29dd7c26d489fe9e5
eoxia-jimmy <jimmy.eoxia@gmail.com>
2018-03-05 09:48:55 +0100

    1.0.0: Appelle le constructeur parent pour init le tableau des callbacks

--- modules/wpeo-model/class/comment.class.php
+++ modules/wpeo-model/class/comment.class.php
@@ -91,6 +91,8 @@ if ( ! class_exists( '\eoxia\Comment_Class' ) ) {
 		 * @return void
 		 */
 		protected function construct() {
+			parent::construct();
+
 			if ( ! in_array( $this->get_type(), \eoxia\Config_Util::$init['eo-framework']->not__in_display_comment ) ) {
 				add_action( 'pre_get_comments', array( $this, 'callback_pre_get_comments' ) );
 			}
