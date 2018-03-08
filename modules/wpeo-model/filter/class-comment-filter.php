51d4a1828fa23ae6d6d91f378efe11e78291ecc0
eoxia-jimmy <jimmy.eoxia@gmail.com>
2018-03-08 10:07:24 +0100

    1.0.0: Exclus les custom comments de la métabox "Activité" du dashboard

--- modules/wpeo-model/filter/class-comment-filter.php
+++ modules/wpeo-model/filter/class-comment-filter.php
@@ -24,8 +24,31 @@ class Comment_Filter {
 	 * Initialisation et appel des différents filtres définis dans EO_Framework.
 	 */
 	public function __construct() {
+		add_filter( 'comments_clauses', array( $this, 'exclude_custom_comments_in_dashboard' ), 10, 2 );
+
 		add_filter( 'eo_model_comment_after_put', array( $this, 'after_save_comment' ), 5, 2 );
 		add_filter( 'eo_model_comment_after_post', array( $this, 'after_save_comment' ), 5, 2 );
+	}
+
+	/**
+	 * Exclus les custom comments de la métabox "Activité" du tableau de bord de WordPress.
+	 *
+	 * @since 1.0.0
+	 * @version 1.0.0
+	 *
+	 * @param  array            $clauses Les paramètres de la requête pour récupérer les Commentaires.
+	 * @param  WP_Comment_Query $query   La query.
+	 *
+	 * @return array                     Les paramètres de la requête avec le comment_type en plus.
+	 */
+	function exclude_custom_comments_in_dashboard( $clauses, $query ) {
+		global $pagenow;
+
+		if ( 'index.php' == $pagenow ) {
+			$clauses['where'] .= ' AND comment_type IN("' . implode( \eoxia\Config_Util::$init['eo-framework']->not__in_display_comment, '","' ) . '")';
+		}
+
+		return $clauses;
 	}
 
 	/**
