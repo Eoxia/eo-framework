d235ff39cf88a7afcbdd4e8eee4b66e1a06a731a
Alexandre Techer - Eoxia <alexandre.techer@eoxia.com>
2018-03-16 15:15:16 +0100

    1.0.0: DropDown events

--- core/assets/js/dest/wpeo-assets.js
+++ core/assets/js/dest/wpeo-assets.js
@@ -606,6 +606,7 @@ if ( ! window.eoxiaJS.dropdown  ) {
 		jQuery( document ).on( 'keyup', window.eoxiaJS.dropdown.keyup );
 		jQuery( document ).on( 'click', '.wpeo-dropdown:not(.dropdown-active) .dropdown-toggle:not(.disabled)', window.eoxiaJS.dropdown.open );
 		jQuery( document ).on( 'click', '.wpeo-dropdown.dropdown-active .dropdown-content', function(e) { e.stopPropagation() } );
+		jQuery( document ).on( 'click', '.wpeo-dropdown.dropdown-active .dropdown-content .dropdown-item', window.eoxiaJS.dropdown.close  );
 		jQuery( document ).on( 'click', '.wpeo-dropdown.dropdown-active', function ( e ) { window.eoxiaJS.dropdown.close( e ); e.stopPropagation(); } );
 		jQuery( document ).on( 'click', 'body', window.eoxiaJS.dropdown.close );
 	};
