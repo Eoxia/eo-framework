aabb53c40c11f29c642caa6497593e9470a935db
eoxia-jimmy <jimmy.eoxia@gmail.com>
2018-03-23 15:27:18 +0100

    1.0.0: add data-time attribute

--- core/assets/js/date.lib.js
+++ core/assets/js/date.lib.js
@@ -11,16 +11,30 @@ if ( ! window.eoxiaJS.date ) {
 
 	window.eoxiaJS.date.init = function() {
 		jQuery( document ).on ('click', '.group-date .date', function( e ) {
-			var defaultDate = jQuery( this ).closest( '.group-date' ).find( '.mysql-date' ).val() ?  jQuery( this ).closest( '.group-date' ).find( '.mysql-date' ).val() : new Date();
+			var format = 'd/m/Y';
+			var timepicker = false;
+
+			if ( jQuery( this ).closest( '.group-date' ).data( 'time' ) ) {
+				format += ' H:i:s';
+				timepicker = true;
+			}
+
 			jQuery( this ).datetimepicker( {
 				lang: 'fr',
-				format: 'd/m/Y',
+				format: format,
 				mask: true,
-				timepicker: false,
-				startDate: defaultDate,
+				timepicker: timepicker,
 				closeOnDateSelect: true,
 				onChangeDateTime : function(ct, $i) {
-					$i.closest( '.group-date' ).find( '.mysql-date' ).val( ct.dateFormat('Y-m-d') );
+					if ( $i.closest( '.group-date' ).data( 'time' ) ) {
+						$i.closest( '.group-date' ).find( '.mysql-date' ).val( ct.dateFormat('Y-m-d H:i:s') );
+					} else {
+						$i.closest( '.group-date' ).find( '.mysql-date' ).val( ct.dateFormat('Y-m-d') );
+					}
+
+					if ( $i.closest( '.group-date' ).attr( 'data-namespace' ) && $i.closest( '.group-date' ).attr( 'data-module' ) && $i.closest( '.group-date' ).attr( 'data-after-method' ) ) {
+						window.eoxiaJS[$i.closest( '.group-date' ).attr( 'data-namespace' )][$i.closest( '.group-date' ).attr( 'data-module' )][$i.closest( '.group-date' ).attr( 'data-after-method' )]( $i );
+					}
 				}
 			} ).datetimepicker( 'show' );
 		});
