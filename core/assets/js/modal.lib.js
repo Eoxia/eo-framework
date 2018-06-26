c68aaa6ef052621d0a003618092c77552590ec22
eoxia-jimmy <jimmy.eoxia@gmail.com>
2018-06-26 10:19:45 +0200

    1.0.0: Ajout d'un trigger

--- core/assets/js/modal.lib.js
+++ core/assets/js/modal.lib.js
@@ -183,6 +183,8 @@ if ( ! window.eoxiaJS.modal  ) {
 						if ( window.eoxiaJS.refresh ) {
 							window.eoxiaJS.refresh();
 						}
+
+						triggeredElement[0].modalElement.trigger( 'modal-opened', triggeredElement );
 					}
 				} );
 			});
@@ -197,6 +199,9 @@ if ( ! window.eoxiaJS.modal  ) {
 			target.addClass( 'modal-active' );
 			target[0].typeModal = 'default';
 			triggeredElement[0].modalElement = target;
+
+			target.trigger( 'modal-opened', triggeredElement );
+
 		}
 
 		event.stopPropagation();
