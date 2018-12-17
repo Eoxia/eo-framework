033ec02b3a4d42778be1ac451cce5cda1faf88a7
Amandine Eoxia <amandine@eoxia.com>
2018-12-17 17:51:40 +0100

    Mise à jour Gulpfile pour le dossier Assets

--- core/assets/gulpfile.js
+++ core/assets/gulpfile.js
@@ -1,56 +1,33 @@
-var gulp = require('gulp');
-var please = require('gulp-pleeease');
-var watch = require('gulp-watch');
-var plumber = require('gulp-plumber');
-var rename = require("gulp-rename");
-var cssnano = require('gulp-cssnano');
+'use strict';
+
+var gulp   = require('gulp');
+var watch  = require('gulp-watch');
 var concat = require('gulp-concat');
-var glob = require("glob");
-var uglify = require('gulp-uglify');
-var sass = require('gulp-sass');
-var stripCssComments = require('gulp-strip-css-comments');
+var sass   = require('gulp-sass');
+var rename = require('gulp-rename');
 
 var paths = {
-    scss: [ 'css/scss/**/*.scss', 'css/' ],
-    css: [ 'css/style.css' ],
- js: ['js/init.js', 'js/*.lib.js'],
+	scss: ['css/scss/**/*.scss', 'css/'],
+	js: ['js/init.js', 'js/*.lib.js'],
 };
 
-gulp.task('build_scss', function() {
-    gulp.src(paths.scss[0])
-        .pipe( sass().on( 'error', sass.logError ) )
-        .pipe(stripCssComments())
-        .pipe(please({
-            minifier: false,
-            autoprefixer: {"browsers": ["last 40 versions", "ios 6", "ie 9"]},
-            pseudoElements: true,
-            sass: true
-        }))
-        .pipe( gulp.dest( paths.scss[1] ) );
+// SCSS Plugin
+gulp.task( 'build_scss', function() {
+	return gulp.src( paths.scss[0] )
+		.pipe( sass().on( 'error', sass.logError ) )
+		.pipe( gulp.dest( paths.scss[1] ) )
+		.pipe( sass({outputStyle: 'compressed'}).on( 'error', sass.logError ) )
+		.pipe( rename( './style.min.css' ) )
+		.pipe( gulp.dest( paths.scss[1] ) );
 });
 
-gulp.task('build_scss_min', function() {
-    gulp.src(paths.css[0])
-        .pipe(stripCssComments())
-        .pipe(please({
-            minifier: true,
-            autoprefixer: {"browsers": ["last 40 versions", "ios 6", "ie 9"]},
-            pseudoElements: true,
-            sass: true,
-            out: 'style.min.css'
-        }))
-        .pipe( gulp.dest( paths.scss[1] ) );
-});
-
-
-gulp.task('build_js_min', function() {
+gulp.task('build_js', function() {
     return gulp.src(paths.js)
         .pipe(concat('wpeo-assets.js'))
         .pipe(gulp.dest('js/dest/'))
 })
 
-gulp.task('default', function() {
-    gulp.watch(paths.scss[0], ["build_scss"]);
-    gulp.watch(paths.scss[0], ["build_scss_min"]);
-    gulp.watch(paths.js, ["build_js_min"]);
+gulp.task( 'default', function() {
+	gulp.watch( paths.scss[0], gulp.series('build_scss') );
+	gulp.watch( paths.js, gulp.series('build_js') );
 });
