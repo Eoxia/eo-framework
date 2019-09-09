d2bff261e898c3db19c3841a09c39ff7289e238b
Jimmy <jimmy.eoxia@gmail.com>
2019-09-09 09:28:50 +0200

    Fix uploda

--- modules/wpeo-upload/action/wpeo-upload.action.php
+++ modules/wpeo-upload/action/wpeo-upload.action.php
@@ -125,6 +125,8 @@ if ( ! class_exists( '\eoxia\WPEO_Upload_Action' ) ) {
 				$filename_only = basename( $filelink );
 				$file          = get_post( $data['file_id'] );
 				$fileurl_only  = $file->guid;
+				$field_name    = $data['field_name'];
+				$file_id       = $data['file_id'];
 
 				ob_start();
 				require \eoxia\Config_Util::$init['eo-framework']->wpeo_upload->path . '/view/' . $data['display_type'] . '/list-item.view.php';
