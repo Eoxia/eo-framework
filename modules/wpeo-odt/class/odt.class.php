162c23bab1bb2a98df51ac9d432e35273ad4b1a4
Jimmy <jimmy.eoxia@gmail.com>
2019-11-14 16:11:38 +0100

    Add Document ODT Type

--- modules/wpeo-odt/class/odt.class.php
+++ modules/wpeo-odt/class/odt.class.php
@@ -33,7 +33,7 @@ if ( ! class_exists( '\eoxia\ODT_Class' ) ) {
 		 *
 		 * @var string
 		 */
-		protected $type = 'attachment';
+		protected $type = 'document-odt';
 
 		/**
 		 * Le type du post
@@ -160,7 +160,7 @@ if ( ! class_exists( '\eoxia\ODT_Class' ) ) {
 				'post_status'    => 'inherit',
 				'posts_per_page' => 1,
 				'tax_query'      => $tax_query,
-				'post_type'      => 'attachment',
+				'post_type'      => array( 'attachment', 'document-odt' ),
 			) );
 
 			// Récupères le document
